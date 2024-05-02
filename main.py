a71f60d9989499b2691ed6f28b7837bfcd86d87f
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2024-05-02 12:13:00 +0530

    Prepare Base Model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chestCancer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chestCancer import logger
+from chestCancer.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -9,6 +10,21 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+
+STAGE_NAME = "Prepare Base Model stage"
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
